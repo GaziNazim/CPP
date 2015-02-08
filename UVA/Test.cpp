4bf56c8622e088c5ff670cd32af369ca2e7ce5ee
GaziNazim <gazinazim1@gmail.com>
2015-02-08 19:17:59 +0600

    08-02-2015

--- UVA/Test.cpp
+++ UVA/Test.cpp
@@ -1,92 +1,21 @@
-#include<iostream>
-#include<cstdio>
-#define SIZE 10
-int q[SIZE], rear=-1, fro=-1,item;
-void new_value();
-void delete_value();
-void show();
-main()
+#include <stdio.h>
+int main()
 {
-    int n=0;
-    while(n!=4)
-    {
-        printf("\n\n1.\tInsert\n2.\tDelete\n3.\tDisplay\n4.\tExit\n");
-        printf("\nEnter your choice: ");
-        scanf("%d", &n);
+    int n,num[5],a,i,j,avg;
 
-        switch(n)
+    scanf("%d",&n);
+    for(j=0; j<n; ++j)
+    {
+        for(a=0,i=0; a<5; a++)
         {
-        case 1:
-            new_value();
-            break;
-        case 2:
-            delete_value();
-            break;
-        case 3:
-            show();
-            break;
 
-        default:
-            printf("\n\nInvalid choice.\n");
+            scanf("%d", &num[a]);
+            i+=num[a];
+            avg=i/5;
         }
+
+        printf("Average %d\n",avg);
     }
-}
 
-void new_value()
-{
-    if((fro==0 && rear==SIZE-1) || (fro==rear+1))
-        printf("\n\nQueue is full.");
-    else
-    {
-        printf("\n\nEnter ITEM: ");
-        scanf("%d", &item);
-
-        if(rear == -1)
-        {
-            rear = 0;
-            fro = 0;
-        }
-        else if(rear == SIZE-1)
-            rear = 0;
-        else
-            rear++;
-
-        q[rear] = item;
-        printf("\n\nItem inserted: %d\n", item);
-    }
-}
-void delete_value()
-{
-    if(fro == -1)
-        printf("\n\nQueue is empty.\n");
-    else
-    {
-        item = q[fro];
-
-        if(fro == rear)
-        {
-            fro = -1;
-            rear = -1;
-        }
-        else if(fro == SIZE-1)
-            fro = 0;
-        else
-            fro++;
-
-        printf("\n\nITEM deleted: %d", item);
-    }
-}
-void show()
-{
-    int i;
-
-    if((fro == -1) || (fro==rear+1))
-        printf("\n\nQueue is empty.\n");
-    else
-    {
-        printf("\n\n");
-
-        for(i=fro; i<=rear; i++)
-            printf("\t%d",q[i]);
-    }
+    return 0;
 }
